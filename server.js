f8eea903ad16421e91d6026d094b978a0710d316
Valya <valya.okulova@mail.ru>
2019-10-19 10:43:07 +0700

    hello, world!

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 let express = require('express');
 let server = express();
 
-server.get('/', function(req, res){
-    res.send('Hello, HELL!');
+server.listen(8080, function(){
+    console.log('IT\'S WORKING!');
 })
 
-server.listen(8080, function(){
-    console.log('IT\'WORKING!');
+let list = [
+{
+    id: 1,
+    name: 'Bob',
+    sex: 'Male',
+    age: 18
+},
+{
+    id: 2,
+    name: 'Katty',
+    sex: 'Female',
+    age: 20
+},
+{
+    id: 3,
+    name: 'Sam',
+    sex: 'Male',
+    age: 21
+},
+];
+
+
+server.get('/get/people', function(req, res){
+    res.send(list);
+})
+
+server.get('/get/people/:id', function(req, res){
+    let human = list.find(function (human){
+        return human.id === Number(req.params.id)
+    })
+    res.send(human);
+})
+
+server.get('/get/people/:id/:field', function(req, res){
+    let field = req.params.field;
+    let human = list.find(function (human){
+        return (human.id == Number(req.params));
+    })
+    res.send(human.field);
 })
