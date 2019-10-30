3982c13d20a1eaef299a48ec459941c9b85e4bd2
Valya <49086658+Narfisa@users.noreply.github.com>
2019-10-30 15:39:35 +0700

    adding Readme, cors, a few queries

---
+++ README.md
@@ -0,0 +1,19 @@
+# crm
+
+## First: install node js
+```
+See [Configuration Reference](https://nodejs.org/en/).
+```
+
+### Second: install express server to project folder with
+'''
+npm install express --save
+'''
+
+### Third: Run this one in terminal in the project folder
+'''
+node server.js
+'''
+
+
+### Follow my node js repository to see front for this project
--- server.js
+++ server.js
@@ -1,47 +1,96 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+let cors = require('cors');
+
 let server = express();
 
-server.listen(8080, function(){
-    console.log('IT\'S WORKING!');
+server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+server.listen(8081, function(){
+  console.log('IT\'S WORKING!');
 })
 
-let list = [
-{
+var dataset = [
+  {
+    id: 0,
+    firstName: 'John',
+    lastName: 'Johny',
+    age: 18,
+    gender: 'Male',
+    url: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+  },
+  {
     id: 1,
-    name: 'Bob',
-    sex: 'Male',
-    age: 18
-},
-{
+    firstName: 'Ann',
+    lastName: 'Anny',
+    age: 17,
+    gender: 'Female',
+    url: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+  },
+  {
     id: 2,
-    name: 'Katty',
-    sex: 'Female',
-    age: 20
-},
-{
+    firstName: 'Kate',
+    lastName: 'Katty',
+    age: 21,
+    gender: 'Female',
+    url: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+  },
+  {
     id: 3,
-    name: 'Sam',
-    sex: 'Male',
-    age: 21
-},
+    firstName: 'Sam',
+    lastName: 'Sammy',
+    age: 19,
+    gender: 'Male',
+    url: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+  },
+  {
+    id: 4,
+    firstName: 'Alex',
+    lastName: 'Alexy',
+    age: 29,
+    gender: 'Male',
+    url: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+  }
 ];
 
-
-server.get('/get/people', function(req, res){
-    res.send(list);
+server.get('/profiles', function(req, res){
+  res.send(dataset);
 })
 
-server.get('/get/people/:id', function(req, res){
-    let human = list.find(function (human){
-        return human.id === Number(req.params.id)
-    })
-    res.send(human);
+server.get('/profile/:id', function(req, res){
+  let human = dataset.find(function (human){
+    return human.id === Number(req.params.id)
+  })
+  res.send(human);
 })
 
-server.get('/get/people/:id/:field', function(req, res){
-    let field = req.params.field;
-    let human = list.find(function (human){
-        return (human.id == Number(req.params));
-    })
-    res.send(human.field);
+server.post('/profiles', function(req, res){
+  let profile = {
+    id: Date.now(),
+    firstName: req.body.firstName,
+    age: req.body.age,
+    gender: req.body.gender
+  }
+  Data.push(profile);
+  console.log(req.body);
+  res.send(profile);
+})
+
+server.put('/profiles/:id', function(req,res){
+  let human = dataset.find(function (human){
+    return human.id === Number(req.params.id)
+  })
+  human.firstName = req.body.firstName;
+  human.age = req.body.age;
+  human.gender = req.body.gender;
+  res.sendStatus(200);
+})
+
+server.delete('/delete/profiles/:id', function(req, res){
+  Data.splice(Number(req.body.id),1);
+  res.send(200);
 })
